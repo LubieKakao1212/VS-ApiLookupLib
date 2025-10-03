db577fa7c12989fb12555422fed89463216f1230
LubieKakao1212 <grzegorz.gomulinski@gmail.com>
2025-10-03 22:35:05 +0200

    Fixed modid for ApiLookupLib + Set up launch settings to handle more projects

--- ApiLookupLib/ApiLookupLibModSystem.cs
+++ ApiLookupLib/ApiLookupLibModSystem.cs
@@ -3,24 +3,12 @@ using Vintagestory.API.Server;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common;
 
-[assembly: ModInfo("Api-Lookup Lib", "ApiLookupLib",
+[assembly: ModInfo("Api-Lookup Lib", "apilookuplib",
     Authors = new string[] { "LubieKakao1212" },
     Description = "TODO",
     Version = "1.0.0")]
 
 namespace ApiLookupLib {
     public class ApiLookupLibModSystem : ModSystem {
-        
-        public override void Start(ICoreAPI api) {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
-        }
-
-        public override void StartServerSide(ICoreServerAPI api) {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("ApiLookupLib:hello"));
-        }
-
-        public override void StartClientSide(ICoreClientAPI api) {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("ApiLookupLib:hello"));
-        }
     }
 }
